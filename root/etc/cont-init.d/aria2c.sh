52b81c4cafde2cd2d3acbcb713221d316d375be0
awsl1414 <3030994569@qq.com>
2024-12-15 17:37:40 +0800

    refactor: :recycle: 脚本语法优化

--- root/etc/cont-init.d/aria2c.sh
+++ root/etc/cont-init.d/aria2c.sh
@@ -34,15 +34,15 @@ fi
 ARIA2_RPC_SECRET_CONF_VAL=$(grep ^rpc-secret= /config/aria2.conf | sed 's/\(.*\)=//g' | sed ":a;N;s/\n//g;ta")
 if [ -n "$ARIA2_RPC_SECRET" ]; then
   if [ "$ARIA2_RPC_SECRET_CONF_VAL" != "$ARIA2_RPC_SECRET" ]; then
-    if [ `grep ^rpc-secret= /config/aria2.conf | wc -l` -gt 0 ]; then
+    if [ "$(grep -c ^rpc-secret= /config/aria2.conf)" -gt 0 ]; then
       sed -i 's/^rpc-secret='"$ARIA2_RPC_SECRET_CONF_VAL"'/rpc-secret='"$ARIA2_RPC_SECRET"'/g' /config/aria2.conf
     else
-      sed -i '$arpc-secret='"$ARIA2_RPC_SECRET"'' /config/aria2.conf
+      sed -i "$ a rpc-secret=$ARIA2_RPC_SECRET" /config/aria2.conf
     fi
   fi
 else
-  if [ ! -n "$ARIA2_RPC_SECRET_CONF_VAL" ] && [ `grep ^rpc-secret= /config/aria2.conf | wc -l` -gt 0 ]; then
-     sed -i 's/^rpc-secret=/\#rpc-secret=/g' /config/aria2.conf
+  if [ -z "$ARIA2_RPC_SECRET_CONF_VAL" ] && [ "$(grep -c ^rpc-secret= /config/aria2.conf)" -gt 0 ]; then
+    sed -i 's/^rpc-secret=/\#rpc-secret=/g' /config/aria2.conf
   fi
 fi
 
@@ -50,26 +50,27 @@ fi
 #listen-port and dht-listen-port
 ARIA2_LISTEN_PORT_CONF_VAL=$(grep ^listen-port= /config/aria2.conf | sed 's/\(.*\)=//g' | sed ":a;N;s/\n//g;ta")
 ARIA2_DHT_LISTEN_PORT_CONF_VAL=$(grep ^dht-listen-port= /config/aria2.conf | sed 's/\(.*\)=//g' | sed ":a;N;s/\n//g;ta")
-if [ -n "$ARIA2_LISTEN_PORT" ] && ([ "$ARIA2_LISTEN_PORT_CONF_VAL" != "$ARIA2_LISTEN_PORT" ] || [ "$ARIA2_DHT_LISTEN_PORT_CONF_VAL" != "$ARIA2_LISTEN_PORT" ]); then
-  if [ `grep ^listen-port= /config/aria2.conf | wc -l` -gt 0 ]; then
+if [ -n "$ARIA2_LISTEN_PORT" ] && { [ "$ARIA2_LISTEN_PORT_CONF_VAL" != "$ARIA2_LISTEN_PORT" ] || [ "$ARIA2_DHT_LISTEN_PORT_CONF_VAL" != "$ARIA2_LISTEN_PORT" ]; }; then
+
+  if [ "$(grep -c ^listen-port= /config/aria2.conf)" -gt 0 ]; then
     sed -i 's/^listen-port='"$ARIA2_LISTEN_PORT_CONF_VAL"'/listen-port='"$ARIA2_LISTEN_PORT"'/g' /config/aria2.conf
   else
-    sed -i '$alisten-port='"$ARIA2_LISTEN_PORT"'' /config/aria2.conf
+    sed -i "$ a listen-port=$ARIA2_LISTEN_PORT" /config/aria2.conf
   fi
-  if [ `grep ^dht-listen-port= /config/aria2.conf | wc -l` -gt 0 ]; then
+  if [ "$(grep -c ^dht-listen-port= /config/aria2.conf)" -gt 0 ]; then
     sed -i 's/^dht-listen-port='"$ARIA2_DHT_LISTEN_PORT_CONF_VAL"'/dht-listen-port='"$ARIA2_LISTEN_PORT"'/g' /config/aria2.conf
   else
-    sed -i '$adht-listen-port='"$ARIA2_LISTEN_PORT"'' /config/aria2.conf
+    sed -i "$ a dht-listen-port=$ARIA2_LISTEN_PORT" /config/aria2.conf
   fi
 fi
 
 #rpc-listen-port
 ARIA2_RPC_LISTEN_PORT_CONF_VAL=$(grep ^rpc-listen-port= /config/aria2.conf | sed 's/\(.*\)=//g' | sed ":a;N;s/\n//g;ta")
 if [ -n "$ARIA2_RPC_LISTEN_PORT" ] && [ "$ARIA2_RPC_LISTEN_PORT_CONF_VAL" != "$ARIA2_RPC_LISTEN_PORT" ]; then
-  if [ `grep ^rpc-listen-port= /config/aria2.conf | wc -l` -gt 0 ]; then
+  if [ "$(grep -c ^rpc-listen-port= /config/aria2.conf)" -gt 0 ]; then
     sed -i 's/^rpc-listen-port='"$ARIA2_RPC_LISTEN_PORT_CONF_VAL"'/rpc-listen-port='"$ARIA2_RPC_LISTEN_PORT"'/g' /config/aria2.conf
   else
-    sed -i '$arpc-listen-port='"$ARIA2_RPC_LISTEN_PORT"'' /config/aria2.conf
+    sed -i "$ a rpc-listen-port=$ARIA2_RPC_LISTEN_PORT" /config/aria2.conf
   fi
 fi
 
@@ -77,12 +78,12 @@ fi
 cp /usr/local/aria2/AriaNg/js/defaultsjs/aria-ng* /usr/local/aria2/AriaNg/js/
 if [ "$ARIANG_RPC_SECRET_AUTO" == "true" ]; then
   if [ -n "$ARIA2_RPC_SECRET" ]; then
-    ARIA2_RPC_SECRET_ARIANg_VAL=`echo -n $ARIA2_RPC_SECRET|base64`
+    ARIA2_RPC_SECRET_ARIANg_VAL=$(echo -n "$ARIA2_RPC_SECRET" | base64)
     sed -i 's/secret:""/secret:"'"$ARIA2_RPC_SECRET_ARIANg_VAL"'"/g' /usr/local/aria2/AriaNg/js/aria-ng*
   else
     ARIA2_RPC_SECRET_CONF_VAL=$(grep ^rpc-secret= /config/aria2.conf | sed 's/\(.*\)=//g' | sed ":a;N;s/\n//g;ta")
     if [ -n "$ARIA2_RPC_SECRET_CONF_VAL" ]; then
-      ARIA2_RPC_SECRET_ARIANg_VAL=`echo -n $ARIA2_RPC_SECRET_CONF_VAL|base64`
+      ARIA2_RPC_SECRET_ARIANg_VAL=$(echo -n "$ARIA2_RPC_SECRET_CONF_VAL" | base64)
       sed -i 's/secret:""/secret:"'"$ARIA2_RPC_SECRET_ARIANg_VAL"'"/g' /usr/local/aria2/AriaNg/js/aria-ng*
     fi
   fi
@@ -101,8 +102,8 @@ if [ "$ARIANG_RPC_LISTEN_PORT_AUTO" == "true" ]; then
 fi
 
 #设定trackers更新任务
-if [ `grep -c updatetrackers.sh /var/spool/cron/crontabs/root` -eq 0 ]; then
-  echo "0       0       *       *       *       /usr/local/aria2/updatetrackers.sh" >> /var/spool/cron/crontabs/root
+if [ "$(grep -c updatetrackers.sh /var/spool/cron/crontabs/root)" -eq 0 ]; then
+  echo "0       0       *       *       *       /usr/local/aria2/updatetrackers.sh" >>/var/spool/cron/crontabs/root
   echo trackers更新任务已设定。
 else
   echo trackers更新任务已存在。
@@ -114,8 +115,8 @@ if [ "$ARIA2_TRACKERS_UPDATE_AUTO" == "true" ]; then
 fi
 
 #设置时区
-ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-echo $TZ > /etc/timezone
+ln -sf /usr/share/zoneinfo/"$TZ" /etc/localtime
+echo "$TZ" >/etc/timezone
 
 #修改用户UID GID
 groupmod -o -g "$GID" aria2
